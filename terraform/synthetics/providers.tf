e19c08fc501f0bfbb5d0003e7eaaa2aa6b2ff945
Kirill <kirilll@cybcube.com>
2026-03-10 13:39:45 +0100

    init commit

---
+++ terraform/synthetics/providers.tf
@@ -0,0 +1,6 @@
+provider "grafana" {
+    url             = "https://cyberandcubes.grafana.net/"
+    auth            = var.grafana_service_token
+    sm_url          = "https://synthetic-monitoring-api-eu-west-2.grafana.net"
+    sm_access_token = var.sm_access_token
+}
